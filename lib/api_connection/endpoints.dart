0a0c41fa12467c44253b9c995f08109907d7c1b4
Jahnics <jahnics1@gmail.com>
2023-06-16 18:00:56 +0200

    base url

--- lib/api_connection/endpoints.dart
+++ lib/api_connection/endpoints.dart
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://15aa-137-64-0-28.ngrok-free.app/";
+const BASE_URL = "https://ict7.pythonanywhere.com/";
 const API_BASE_URL = "${BASE_URL}api";
 const TOKEN_URL = "$API_BASE_URL/api-token-auth/";
 const REGISTER_URL = "$API_BASE_URL/users/register/";
