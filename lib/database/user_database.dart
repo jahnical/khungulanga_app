84a1284a78f862e9a3d406e3075eb9d48a896d85
habilahamza <bed-com-45-18@cc.ac.mw>
2023-06-29 16:39:58 +0200

    database table for users

--- lib/database/user_database.dart
+++ lib/database/user_database.dart
@@ -5,6 +5,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+//database
 const userTable = 'userTable';
 
 class DatabaseProvider {
